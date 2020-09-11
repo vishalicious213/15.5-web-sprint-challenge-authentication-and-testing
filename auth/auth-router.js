9a18507d36a780c68c21cab717d79813ffaefda7
Vish Singh <vishalicious213@gmail.com>
2020-09-11 16:52:56 -0700

    auth-router: REGISTER user works! password hashed with bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Users = require('./auth-model')
+const bcrypt = require('bcryptjs')
 
 // GET all users
 // router.get('/users', usersMiddleware.restrict(), async (req, res, next) => {
@@ -11,8 +12,27 @@ const Users = require('./auth-model')
     }
 })
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res, next) => {
   // implement registration
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		const newUser = await Users.add({
+			username,
+      password: await bcrypt.hash(password, 14)
+		})
+
+		res.status(201).json(newUser)
+	} catch(err) {
+		next(err)
+	}
 });
 
 router.post('/login', (req, res) => {
