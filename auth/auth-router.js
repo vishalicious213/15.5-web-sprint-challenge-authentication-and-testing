3c39d3591dd1906b3b938d8ffc897aeab5520334
Vish Singh <vishalicious213@gmail.com>
2020-09-11 16:44:37 -0700

    added .env file. auth-router: GET all users works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+const Users = require('./auth-model')
+
+// GET all users
+// router.get('/users', usersMiddleware.restrict(), async (req, res, next) => {
+  router.get('/users', async (req, res, next) => {
+    try {
+        res.json(await Users.findAll())
+    } catch(error) {
+        next(error)
+    }
+})
 
 router.post('/register', (req, res) => {
   // implement registration
