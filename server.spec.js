1999ab501f55d970a9fea83422823e441fb21e4c
Vish Singh <vishalicious213@gmail.com>
2020-09-13 14:43:14 -0700

    test: fail to log in user & test: fail to register user added

--- server.spec.js
+++ server.spec.js
@@ -21,13 +21,42 @@ describe("login", () => {
 	});
 });
 
+describe("fail login", () => {
+	it('should not log user in (wrong password)', async () => {
+		const res = await request(server)
+			.post("/api/auth/login")
+			.send({ username: "vish", password: "password-2" });
+		expect(res.statusCode).toBe(401);
+		expect(res.type).toBe("application/json");
+		expect(res.body.message).toBe("You shall not pass!");
+	});
+});
+
 describe("register new user", () => {
-	it('should add a new user', async () => {
+	it('should add a new user if user does not exist', async () => {
 		const res = await request(server)
 			.post("/api/auth/register")
 			.send({ username: "ella", password: "password-2" });
+		// if user does not exist
 		expect(res.statusCode).toBe(201);
 		expect(res.type).toBe("application/json");
 		expect(res.body.username).toBe("ella");
 	});
 });
+
+describe("fail to register new user", () => {
+	it('should not add a new user if user exists', async () => {
+		const res = await request(server)
+			.post("/api/auth/register")
+			.send({ username: "ella", password: "password-2" });
+		// if user exists
+		expect(res.statusCode).toBe(409);
+		expect(res.type).toBe("application/json");
+		expect(res.body.message).toBe("Username is already taken");
+	});
+});
+
+// close out db so process doesn't hang
+afterAll(async () => {
+	await db.destroy()
+})
